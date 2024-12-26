9f751ee799db793b903494da7c287478d6e05a05
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2024-12-26 17:14:44 -0400

    feat: enemy chasing state concluido

--- Assets/Scripts/StateMachines/Enemy/EnemyChasingState.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyChasingState.cs
@@ -1,21 +1,48 @@
+using System;
 using UnityEngine;
 
 public class EnemyChasingState : EnemyBaseState
 {
+    private readonly int LocomotionHash = Animator.StringToHash("Locomotion");
+    private readonly int SpeedHash = Animator.StringToHash("Speed");
+
+
     public EnemyChasingState(EnemyStateMachine stateMachine) : base(stateMachine)
     {
     }
 
     public override void Enter()
     {
-        Debug.Log("In Range");
+        stateMachine.Animator.CrossFadeInFixedTime(LocomotionHash, .1f);
     }
 
     public override void Tick(float deltaTime)
     {
+        if (!IsInChaseRange())
+        {
+            stateMachine.SwitchState(new EnemyIdleState(stateMachine));
+            return;
+        }
+
+        MoveToPlayer(deltaTime);
+
+        stateMachine.Animator.SetFloat(SpeedHash, 1f, .1f, deltaTime);
     }
 
     public override void Exit()
     {
+        stateMachine.Agent.ResetPath();
+        stateMachine.Agent.velocity = Vector3.zero;
     }
+
+    private void MoveToPlayer(float deltaTime)
+    {
+        stateMachine.Agent.destination = stateMachine.Player.transform.position;
+
+        Move(stateMachine.Agent.desiredVelocity.normalized * stateMachine.MovementSpeed, deltaTime);
+
+        // Atualizamos a velocity do agent porque nós estamos lidando com a movimentação manualmente
+        stateMachine.Agent.velocity = stateMachine.CharacterController.velocity;
+    }
+
 }
--- Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStateMachine : StateMachine
 {
     [field: SerializeField] public Animator Animator { get; private set; }
     [field: SerializeField] public CharacterController CharacterController { get; private set; }
+    [field: SerializeField] public NavMeshAgent Agent { get; private set; }
     [field: SerializeField] public ForceReceiver ForceReceiver { get; private set; }
+
+    [field: SerializeField] public float MovementSpeed { get; private set; }
     [field: SerializeField] public float PlayerChasingRange { get; private set; } = 10f;
 
     public PlayerStateMachine Player { get; private set; }
@@ -12,6 +16,9 @@ public class EnemyStateMachine : StateMachine
     private void Start()
     {
         Player = PlayerStateMachine.Instance;
+        
+        Agent.updatePosition = false;
+        Agent.updateRotation = false;
 
         SwitchState(new EnemyIdleState(this));
     }
